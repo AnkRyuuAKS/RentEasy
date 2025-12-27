99f3f1e1008c67b9e89a314bf185db11bc1e4c44
AnkitAKS02 <ankitkumarswain2003@gmail.com>
2025-12-27 17:07:09 +0530

    allowed the review route

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ import  connectDB  from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth_route.js'
 import listingRoutes from './routes/listing_route.js'
-// import reviewRoutes from './routes/review_route.js'
+import reviewRoutes from './routes/review_route.js'
 const app = express();
 const PORT = process.env.PORT || 3002;
 
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/listing', listingRoutes);
-// app.use('/api/review', reviewRoutes);
+app.use('/api/review', reviewRoutes);
 
 
 
